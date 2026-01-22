5ed66b6c638489a1f53821224cd75b6ab628702c
Rafael Delgado <co.rafaeldelgado@gmail.com>
2026-01-22 09:36:58 -0800

    Enable nodejs_compat in wrangler and clean next config

--- next.config.ts
+++ next.config.ts
@@ -29,35 +29,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config: any, { isServer, nextRuntime }: any) => {
-    // Fixes npm packages that depend on `node:` modules
-    if (!isServer || nextRuntime === 'edge') {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        stream: false,
-        crypto: false,
-        buffer: false,
-        util: false,
-        net: false,
-        tls: false,
-        fs: false,
-        child_process: false,
-      };
-
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'node:stream': false,
-        'node:buffer': false,
-        'node:crypto': false,
-        'node:util': false,
-        'node:net': false,
-        'node:tls': false,
-        'node:fs': false,
-        'node:child_process': false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
